3407cc93b89965cfe01cee8fa452627de34496b9
adityagothiwarekar <aditya.gothiwarekar@gmail.com>
2022-08-05 15:37:14 +0530

    changed

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int a,b,sum;
+        a=2;
+        b=5;
+        sum=a+b;
+        System.out.println("sum is" +sum);
+	// write your code here
+    }
+
+}
+
